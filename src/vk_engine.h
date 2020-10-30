ac26a0d666a2c54a03566ad82c1d2956bd82bdaa
Tri M. Cao <trimcao@gmail.com>
2020-10-29 22:58:08 -0700

    initialize vulkan engine and swapchain

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -4,16 +4,39 @@
 #pragma once
 
 #include <vk_types.h>
+#include <vector>
 
 class VulkanEngine {
+private:
+	void init_vulkan();
+
+	void init_swapchain();
+
 public:
 
 	bool _isInitialized{ false };
-	int _frameNumber {0};
+	int _frameNumber{ 0 };
 
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
 	struct SDL_Window* _window{ nullptr };
+
+	VkInstance _instance; // Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU; // GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface; // Vulkan window surface
+
+	VkSwapchainKHR _swapchain;
+
+	// image format expected by the windowing system
+	VkFormat _swapchainImageFormat;
+
+	// array of images from the swapchain
+	std::vector<VkImage> _swapchainImages;
+
+	// array of image-views from the swapchain
+	std::vector<VkImageView> _swapchainImageViews;
 
 	//initializes everything in the engine
 	void init();
